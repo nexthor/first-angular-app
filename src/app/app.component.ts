74c04e476292ddb305fc297c5e9d6a11c613c7bc
Nestor Danilo Mendoza <nestordanilo.mendoza@concentrix.com>
2025-03-15 13:16:25 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title = 'nexthor';
+  algo = 'how are you today?';
 }
