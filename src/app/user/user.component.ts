4be4ca7be7f045031c71e66a7a2386c681fe646f
Nestor Danilo Mendoza <nestordanilo.mendoza@concentrix.com>
2025-03-17 18:26:41 -0600

    new user component now shows random user's info

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randomIndex]
 }
