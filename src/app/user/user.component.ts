d865d4a104ba5e63313729b02710ca30dda6d175
Nestor Danilo Mendoza <nestordanilo.mendoza@concentrix.com>
2025-03-19 16:44:24 -0600

    using output without decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, Input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, Input, output, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,9 +9,10 @@ import { Component, computed, EventEmitter, Input, Output } from '@angular/core'
 export class UserComponent {
   @Input({ required: true }) avatar?: string;
   @Input({ required: true }) name?: string;
-  @Input({ required: true}) id?: string;
+  @Input({ required: true}) id!: string;
 
-  @Output() select = new EventEmitter();
+  //@Output() select = new EventEmitter();
+  select = output<string>();
 
   get imagePath() {
     return `/assets/users/${this.avatar}`;
