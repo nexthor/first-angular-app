547e4e2c76a0e2b0d1243f7f655d25627a53da8f
Nestor Danilo Mendoza <nestordanilo.mendoza@concentrix.com>
2025-03-19 16:54:10 -0600

    adding type of emmiter to prevent some errors when a parameter is assigned

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,9 @@
-import { Component, computed, EventEmitter, Input, output, Output } from '@angular/core';
+import { 
+  Component, 
+  computed, 
+  EventEmitter, 
+  Input, 
+  Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,14 +14,10 @@ import { Component, computed, EventEmitter, Input, output, Output } from '@angul
 export class UserComponent {
   @Input({ required: true }) avatar?: string;
   @Input({ required: true }) name?: string;
-  @Input({ required: true}) id!: string;
+  @Input({ required: true}) id?: string;
 
-  //@Output() select = new EventEmitter();
-  select = output<string>();
-
-  get imagePath() {
-    return `/assets/users/${this.avatar}`;
-  }
+  @Output() select = new EventEmitter<string>();
+  imagePath = computed(() => `/assets/users/${this.avatar}`);
 
   onSelectUser() {
     this.select.emit(this.id);
